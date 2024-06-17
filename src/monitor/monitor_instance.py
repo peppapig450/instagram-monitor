dc12020124112910c84d0d2c344b6252ec82df5f
peppapig450 <nbgenius1@gmail.com>
2024-06-17 11:54:31 -0700

    monitor instance

---
+++ src/monitor/monitor_instance.py
@@ -0,0 +1,29 @@
+from collections import deque
+from datetime import datetime
+from threading import Timer
+import logging
+from monitor.monitor import InstagramMonitor
+from login.login_manager import LoginManager
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+
+class MonitorInstance:
+    def __init__(self, username: str, insta_loader, interval_minutes):
+        self.username = username
+        self.insta_loader = insta_loader
+        self.interval_minutes = interval_minutes
+        self.monitor = InstagramMonitor(username, insta_loader)
+    
+    def run_monitor(self):
+        logging.info("Starting monitor for %s...", self.username)
+        self.monitor.run_monitor()
+        logging.info("Monitor for %s completed. Rescheduling in %d minutes", self.username, self.interval_minutes)
+    
+    def schedule_next_run(self):
+        interval_seconds = self.interval_minutes * 60
+        Timer(interval_seconds, self.run_monitor_and_reschedule).start()
+        
+    def run_monitor_and_reschedule(self):
+        self.run_monitor()
+        self.schedule_next_run()
