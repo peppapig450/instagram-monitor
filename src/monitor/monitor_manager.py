044b4d3039d176a88ffc09caabdccd8eb063a132
peppapig450 <nbgenius1@gmail.com>
2024-06-17 15:57:16 -0700

    on the first username, run immediately as waiting 11 minutes the first time is irrelevant

--- src/monitor/monitor_manager.py
+++ src/monitor/monitor_manager.py
@@ -5,7 +5,7 @@ from .monitor_instance import MonitorInstance
 
 class MonitorManager:
     def __init__(self) -> None:
-        self.monitors = deque()
+        self.monitors: deque[MonitorInstance] = deque()
         self.current_monitor = None
 
     def add_monitor(self, username: str, insta_loader, interval_minutes):
@@ -14,10 +14,16 @@ class MonitorManager:
 
         if len(self.monitors) == 1:
             self.current_monitor = monitor_instance
-            monitor_instance.schedule_next_run()
+            monitor_instance.run_monitor_and_reschedule()
+
+        monitor_instance.schedule_next_run()
 
     def rotate_and_run_next(self):
         # Rotate the deque to run the next monitor
         self.monitors.rotate(-1)
         self.current_monitor = self.monitors[0]
         self.current_monitor.schedule_next_run()
+
+    def stop_all(self):
+        for monitor_instance in self.monitors:
+            monitor_instance.cancel_timer()
